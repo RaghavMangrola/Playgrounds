8da33235feaf754b507520a657170197ddfcdc90
Raghav Mangrola <raghav.mangrola@gmail.com>
2018-05-08 10:24:02 -0700

    Private Setters

---
+++ Private Setters.playground/Contents.swift
@@ -0,0 +1,15 @@
+struct TrackedString {
+    private(set) var numberOfEdits = 0
+    var value: String = "" {
+        didSet {
+            numberOfEdits += 1
+        }
+    }
+}
+
+var stringToEdit = TrackedString()
+stringToEdit.value = "This string will be tracked."
+stringToEdit.value += " This edit will increment numberOfEdits."
+stringToEdit.value += " So will this one."
+print("The number of edits is \(stringToEdit.numberOfEdits)")
+// Prints "The number of edits is 3"
